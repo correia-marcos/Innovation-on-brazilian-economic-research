b6294aa1d3a533985693bef080db49540f5a2384
correia-marcos <marcospaulorcorreia@gmail.com>
2021-04-05 21:29:01 -0300

    improved dataframe and names of scripts

--- 6-further_analysis/dataframe_stata.py
+++ 6-further_analysis/dataframe_stata.py
@@ -43,16 +43,43 @@ df = citations_base[cols]
 
 del cols
 
-df = df[~df.index.duplicated(keep='first')]
+# df = df[~df.index.duplicated(keep='first')]
 
 df_merge = pd.merge(kld, df,
                     left_index=True,
                     right_index=True).drop_duplicates()
 
 df_merge = pd.merge(df_merge, matches, left_index=True, right_index=True)
-
-corr = df_merge.corr()
-
 df_merge.to_excel(f'{root}/dados/outside/base_stata.xlsx')
 
 corr = df_merge.corr()
+
+# =============================================================================
+# For the new plots
+# =============================================================================
+
+
+def get_deprt(lista):
+    """Add together from row with same index."""
+    base = []
+    for item in lista:
+        base.append(item)
+
+    resultado = set(base)
+    return resultado
+
+
+df_merge['papers'] = df_merge.index
+df_merge = df_merge.drop('depts', 1)
+df_merge = df_merge.drop('citations1', 1)
+cols = df_merge.columns.to_list()
+cols.remove('Dept')
+
+
+df_small = df_merge.groupby('papers')['Dept'].apply(lambda x:
+                                                    get_deprt(x)).reset_index()
+df_small.index = df_small.papers
+
+df_merge['Total_depts'] = df_small.Dept
+df_merge = df_merge[~df_merge.index.duplicated(keep='first')]
+df_merge.to_excel(f'{root}/dados/outside/base_stata_complete.xlsx')
